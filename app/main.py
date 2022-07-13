bcbb888b4f627add905932ac0a24ceadb2e0a047
Nolan <nolanwelch@outlook.com>
2022-07-13 10:07:48 -0700

    Remove unused files and add os.chdir() line

--- app/main.py
+++ app/main.py
@@ -2,7 +2,10 @@ import annotate_pdf
 import extract_image_data
 import translate_text
 import output_translations
+import os
 from funcs_classes import TranslationConfig
+
+os.chdir('/Users/nolanwelch/Documents/CAT')
 
 if __name__ == '__main__':
     tr_cfg: TranslationConfig = annotate_pdf.run()
