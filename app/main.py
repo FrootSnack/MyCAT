8d10fb3ac276cdf890260b9a872372332381ebfd
Nolan <nolanwelch@outlook.com>
2022-07-15 12:54:41 -0700

    Remove useless imports

--- app/main.py
+++ app/main.py
@@ -1,9 +1,7 @@
 import annotate_pdf
-from funcs_classes import import_tr_to_translationconfig
 import extract_image_data
 import translate_text
 import output_translations
-import funcs_classes
 from funcs_classes import TranslationConfig
 from os import chdir
 from pathlib import Path
